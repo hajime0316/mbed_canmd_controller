a94b3e0dcd8cbeae40502f70422e57c443cac95a
Hajime <hajime0316@s.okayama-u.ac.jp>
2022-03-22 09:39:38 +0900

    ビルドフォルダを追跡から除外

--- NUCLEO_F303RE/canmd_controller.d
+++
@@ -1,7 +0,0 @@
-NUCLEO_F303RE/canmd_controller.o: canmd_controller.c \
- /home/hajime/dev_mbed/adamgreen-gcc4mbed-0e9c0fb/src/mbed_config.h \
- canmd_controller.h
-
-/home/hajime/dev_mbed/adamgreen-gcc4mbed-0e9c0fb/src/mbed_config.h:
-
-canmd_controller.h:
